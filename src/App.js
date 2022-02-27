3f0951132e920f3db5d09209bbd4cbebde8a34c4
LuisDurand1 <luis-rodrigo2213@hotmail.com>
2022-02-26 23:13:53 -0300

    p and span added in app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div>
+        <p>El diegote</p>
       <h1>Welcome</h1>
+
+      <span>aaaaa</span>
+      </div>
     </div>
   );
 }
